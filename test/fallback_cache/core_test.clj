6681d1bd387eb361a84d46abc59677bb5c5b91b2
Ильдар Фатыхов <i.fatikhov@infotech.team>
2017-07-03 23:37:23 +0300

    removed some odd directories

--- test/fallback_cache/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns fallback-cache.core-test
-  (:require [clojure.test :refer :all]
-            [fallback-cache.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
